08f90cbdd2e58c6f8a33bdd1c48db0afa7f40834
RaelFlores <raelflores123@gmail.com>
2023-09-27 22:16:33 -0300

    primeiro commit

--- App.js
+++ App.js
@@ -4,11 +4,11 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-<<<<<<< HEAD
+
       <Text>gabriel sem vergonha e pequeno</Text>
-=======
+      
       <Text>testando o app novo</Text>
->>>>>>> ee5925abf068037a33fdeda9d2de85f7f511dd89
+
       <StatusBar style="auto" />
     </View>
   );
